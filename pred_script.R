63f9c6ca463261becd1ef8338e57f056255705e6
david l gibbs <gibbsdavidl@gmail.com>
2022-09-13 14:58:08 -0500

    script available

--- pred_script.R
+++ pred_script.R
@@ -1,56 +1,41 @@
----
-title: "Prediction Template"
-output: html_notebook
----
-
-Hopefully this can serve as a guide to using the package with this selection of data.
-
-First to install the package.
-```{r}
+#First to install the package.
 library(readr)
 library(stringr)
 library(devtools)
-if (!require(robencla)) {
-  devtools::install_github("https://github.com/Gibbsdavidl/robencla",quiet = TRUE)
-}
-```
+require(robencla)
 
-```{r}
 ## EDIT THESE LOCATIONS##
+
+###### SHOULD READ FROM CMD LINE ######
 
 ### WHERE THE FORMATTED DATA DIR LIVES ###
 data_dir <- 'D:/Work/cluster_prediction_data/formatted/'
 
 ### WHERE TO WRITE THE NEW TABLES ###
-out_dir <- paste0(data_dir, 'results/')
+out_dir <- 'D:/Work/cluster_prediction_data/results/'
+
+### MAKE SURE OUTPUT PATH IS OK TO WRITE ###
 if (!dir.exists(out_dir)){
   dir.create(out_dir)
 }
 
 ### WHAT TO NAME THE FILES, WILL BE A COLUMN
-file_prefix <- "Random_Genes_Sept13_run3"
+file_prefix <- "Random_Genes_Sept13_run4"
 
 ### GENES TO INCLUDE AS A FILE ###
 gene_table <- read.csv(paste0(data_dir,'random_genes_table.csv'), header=T, stringsAsFactors = F)
 all_genes <- unique(gene_table$Gene)
-```
 
-
-
-```{r}
 # then create some gene signatures:
 random_sigs <- list(
     sig1=gene_table$Gene[gene_table$Type == 'Set1'],
     sig2=gene_table$Gene[gene_table$Type == 'Set2'],
     sig3=gene_table$Gene[gene_table$Type == 'Set3']
 )
-```
 
-Now we're ready to do some training
-```{r}
-library(robencla)
+#Now we're ready to do some training
 
-# our classifier object named Anne.
+# our classifier object named Susan
 susan <- Robencla$new("susan")
 
 # xgboost parameters to pass to each sub-classifier in the ensembles
@@ -74,31 +59,21 @@ susan$autotrain(data_file=paste0(data_dir,'EBPP_subset.csv'),
                train_perc=0.8,
                combine_function='median')
 
-
-```
-
-
-```{r}
-
 susan$autotest(paste0(data_dir,'Rsubread_subset.csv'),
               label_name='Label',
               sample_id = 'Barcode')
 
+# Write out the predictions for each sample
 pred_results <- susan$results(include_label = T)
 pred_results$RunName <- file_prefix
 pred_results <- apply(pred_results,2,as.character)
-
 write.csv(pred_results, file=paste0(out_dir,file_prefix,'_pred_results.csv'), row.names = F, quote=F)
-```
-```{r}
+
 # metrics on the test set predictions
 pred_metrics <- susan$classification_metrics(use_cv_results = F)  #
-
 pred_metrics$RunName <- file_prefix
-
 write_csv(pred_metrics,file=paste0(out_dir,file_prefix,'_pred_metrics.csv'))
-```
-```{r}
+
 # and get the importance of features in each ensemble member
 imp_list <- susan$importance() # uses the last fold trained.
 imp_table <- data.frame()
@@ -108,12 +83,10 @@ for (li in names(imp_list)){
 }
 imp_table$RunName <- file_prefix
 write_csv(imp_table,file=paste0(out_dir,file_prefix,'_importance.csv'))
-```
 
-```{r}
 # plot the ROC curves for each class
 ## IF THE ROC IS UPSIDE DOWN, SET FLIP=T
 #ensemble_rocs(anne, flip=F) # uses the last fold trained.
-```
 
-The end.
+# END
+
